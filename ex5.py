30eb74d577916eb5eb389008afbc342571e67704
rheshagan <rheasg@gmail.com>
2018-11-25 08:33:12 +0400

    Completed ex5 on format strings

---
+++ ex5.py
@@ -0,0 +1,20 @@
+#All about Format string
+
+my_name = 'Rhea Sharath Ganapathy'
+my_age = 27 #not a lie
+my_height = 177 #cm
+my_weight = 69 #kgs
+my_eyes = 'brown'
+my_teeth = "white"
+my_hair = "black" #naturally
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} kilos heavy.")
+print("Actually not that heavy")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+#Adding string variables
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
