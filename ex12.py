df9db0a4e872e54eae9e2bfb02fea785b82b92a6
rheshagan <rheasg@gmail.com>
2018-11-26 09:52:07 +0400

    ex12 involves including prompts inside input function

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? ") #you can also include a string inside the parantheses of the input function to prompt the user to type the info you need
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
